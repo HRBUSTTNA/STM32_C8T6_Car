552afd697bfda64213bfe5d42b8e219567cc81ab
Cossiant <L861149396@163.COM>
2023-07-23 15:39:31 +0800

    重新更改了cppmain当中的函数 并且重构了PWM设置函数 创建了type.cpp为将来类的内容实现做规划

--- C8T6_CPP/MDK-ARM/include/cppmain.cpp
+++ C8T6_CPP/MDK-ARM/include/cppmain.cpp
@@ -1,15 +1,13 @@
 #include "include.h"
 #include "includecpp.h"//CPP写的函数一定要在这个文件里声明，不要到include.h中声明！
 
-Car_setting Car;
+Car_Setting Car;
 
 void cpp_main() {
-    Car.PWM_NUM = 1;//测试用参数，到时候直接删除就行
+    Car.PWM.Max_Num = 10;
+    Car.PWM.Change_Num = 2;
+    Car.PWM.Now_Time = 8;
 }
 
 void cpp_while_main() {
-    Car.PWM_NUM = 1;
-    HAL_Delay(500);
-    Car.PWM_NUM = 0;
-    HAL_Delay(500);
 }
--- C8T6_CPP/MDK-ARM/include/type/type.h
+++ C8T6_CPP/MDK-ARM/include/type/type.h
@@ -1,13 +1,17 @@
-//
-// Created by Admin on 2023/7/20 0020.
-//
 
 #ifndef TYPE_H
 #define TYPE_H
 
-class Car_setting{
+class PWM_Setting {
 public:
-    unsigned char PWM_NUM;
+    unsigned char Max_Num;
+    unsigned char Change_Num;
+    unsigned char Now_Time;
+};
+
+class Car_Setting {
+public:
+    PWM_Setting PWM;
 };
 
 #endif //C8T6_CPP_TYPE_H
