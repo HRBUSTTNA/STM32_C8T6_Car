f873f49d5912f0df7ea125677fae8894ccbf7b52
Cossiant <L861149396@163.COM>
2023-07-26 15:58:18 +0800

    这个版本实现了小车的整体移动函数的构造，通过对MOVE_UP进行修改来实现小车整体的移动 下个版本要加入定时器，以此来实现精确的PWM定时控制车速

--- C8T6_CPP/MDK-ARM/include/type/type.cpp
+++ C8T6_CPP/MDK-ARM/include/type/type.cpp
@@ -1,9 +1,18 @@
 //
 // Created by Cossiant on 2023/7/23 0023.
+//type.cpp主要内容有type.h部分函数的实现过程
+//1、PWM_Speed_Write
+//2、move_Single
 //
+
 #include "includecpp.h"
 
-void motor_MOVE_Setting::PWM_Speed_Write(unsigned char motor_number, PWM_Setting PWM) {
+/*函数名称:PWM_Speed_Write
+ *传入参数:motor_number,PWM
+ *传入类型:unsigned char ,PWM Setting
+ *用途:使用这个函数对对应编号的电机进行PWM速度设定
+ */
+void mottor_MOVE_Setting::PWM_Speed_Write(unsigned char motor_number, PWM_Setting PWM) {
     switch (motor_number) {
         case 1://设置右后方电机PWM
             if (PWM.Now_Time >= PWM.Change_Num) {
@@ -38,7 +47,12 @@ void motor_MOVE_Setting::PWM_Speed_Write(unsigned char motor_number, PWM_Setting
     }
 }
 
-void motor_MOVE_Setting::move_Single(unsigned char motor_number, unsigned char move_Mod) {
+/*函数名称:move_Single
+ *传入参数:motor_number,moveMod
+ *传入类型:unsigned char ,unsigned char
+ *用途:使用这个函数对对应编号的电机进行运动操控
+ */
+void mottor_MOVE_Setting::move_Single(unsigned char motor_number, unsigned char move_Mod) {
     GPIO_PinState IN1, IN2;
     switch (move_Mod) {
         case 0://停止模式
